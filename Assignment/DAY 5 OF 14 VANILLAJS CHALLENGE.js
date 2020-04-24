93aee54a59dffeb51257f7e60e4d6f6880f8fe32
Young <wook2124@naver.com>
2020-04-24 13:36:37 +0000

    DAY 5 OF 14 VANILLAJS CHALLENGE

--- Assignment/DAY 5 OF 14 VANILLAJS CHALLENGE.js
+++ Assignment/DAY 5 OF 14 VANILLAJS CHALLENGE.js
@@ -1,8 +1,7 @@
 import "./styles.css";
 
 const dDay = document.querySelector(".D-day"),
-  xmas = document.querySelector("h2")
-
+  xmas = document.querySelector("h2");
 
 // You're gonna need this
 const NINE_HOURS_MILLISECONDS = 32400000;
@@ -10,21 +9,25 @@ const NINE_HOURS_MILLISECONDS = 32400000;
 function getTime() {
   // Don't delete this.
   const xmasDay = new Date("2020-12-24:00:00:00+0900");
-  
-/*  const end = new Date();
+
+  /*  const end = new Date();
   const elapsed = end.getTime() - xmasDay.getTime()
   elapsed() */
-  
-  const date =xmasDay.getDate(),
+
+  const date = xmasDay.getDate(),
     hours = xmasDay.getHours(),
     minutes = xmasDay.getMinutes(),
-    seconds = xmasDay.getSeconds()
-  
-  xmas.innerText = `${date < 10 ? `${date}` : date}d ${hours < 10 ? `${hours}` : hours}h ${minutes < 10 ? `${minutes}` : minutes}m ${seconds < 10 ? `${seconds}` : seconds}s`
+    seconds = xmasDay.getSeconds();
+
+  xmas.innerText = `${date < 10 ? `0${date}` : date}d ${
+    hours < 10 ? `0${hours}` : hours
+    }h ${minutes < 10 ? `0${minutes}` : minutes}m ${
+    seconds < 10 ? `0${seconds}` : seconds
+    }s`;
 }
 
 function init() {
   getTime();
-  setInterval(getTime, NINE_HOURS_MILLISECONDS)
+  setInterval(getTime, NINE_HOURS_MILLISECONDS);
 }
 init();
