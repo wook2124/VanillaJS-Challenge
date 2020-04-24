eaa77d4af73655fb76919e3dd2884b72b8757636
Young <wook2124@naver.com>
2020-04-24 08:29:20 +0000

    DAY 5 OF 14 VANILLAJS CHALLENGE

---
+++ Assignment/DAY 5 OF 14 VANILLAJS CHALLENGE.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+// You're gonna need this
+const NINE_HOURS_MILLISECONDS = 32400000;
+
+function getTime() {
+  // Don't delete this.
+  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+}
+
+function init() {}
+init();
