6b1e2b428dc617eb30fb9282a87cd55a45e1050d
Young <wook2124@naver.com>
2020-04-27 14:24:08 +0000

    DAY 6 OF 14 VANILLAJS CHALLENGE

--- Assignment/DAY 6 OF 14 VANILLAJS CHALLENGE.js
+++ Assignment/DAY 6 OF 14 VANILLAJS CHALLENGE.js
@@ -45,3 +45,49 @@ function loadCountry() {
 function init() {}
 
 init();
+
+
+// 정답
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Parcel Sandbox</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    <h1>Where are you from?</h1>
+    <select class="js-select">
+      <option value="NONE">--- Choose Your Country ---</option>
+      <option value="KR">Korea</option>
+      <option value="GR">Greece</option>
+      <option value="TR">Turkey</option>
+      <option value="FI">Finland</option>
+    </select>
+    <script src="src/index.js"></script>
+  </body>
+</html>
+
+
+import "./styles.css";
+
+const select = document.querySelector(".js-select");
+
+function handleChange() {
+  const selected = select.value;
+  localStorage.setItem("country", selected);
+}
+
+function loadCountries() {
+  const selected = localStorage.getItem("country");
+  if (selected) {
+    const option = document.querySelector(`option[value="${selected}"]`);
+    option.selected = true;
+  }
+}
+
+function init() {
+  loadCountries();
+  select.addEventListener("change", handleChange);
+}
+
+init();
