aa6879b5f664ae64ba2eeb0c8f6db2d484da0117
Young <wook2124@naver.com>
2020-04-28 03:09:34 +0000

    #3.7 Making a To Do List part Two

--- todo.js
+++ todo.js
@@ -1,12 +1,23 @@
-// #3.6 8분부터 다시 듣기
-
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
+function filterFn(toDo) {
+  return toDo.id === 1
+}
+
 const toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(filterFn);
+  console.log(cleanToDos)
+}
+
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -19,6 +30,7 @@ function paintToDo(text) {
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
@@ -44,7 +56,10 @@ function handleSumbit(event) {
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
   if (loadedToDos !== null) {
-
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    })
   }
 }
 
