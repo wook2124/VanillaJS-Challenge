dd1c5e3ddc1eaf4c2a6a85989596a52d7e1bc24b
Young <wook2124@naver.com>
2020-04-28 03:16:15 +0000

    #3.7 Making a To Do List part Three

--- todo.js
+++ todo.js
@@ -4,18 +4,17 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-function filterFn(toDo) {
-  return toDo.id === 1
-}
-
-const toDos = [];
+let toDos = [];
 
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
   toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(filterFn);
-  console.log(cleanToDos)
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
 }
 
 
