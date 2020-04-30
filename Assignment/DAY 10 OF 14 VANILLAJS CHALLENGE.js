535ec16a6f6bc6cc0fe7c34a64cf40b8fb944dea
Young <wook2124@naver.com>
2020-04-30 04:11:32 +0000

    DAY 10 OF 14 VANILLAJS CHALLENGE

--- Assignment/DAY 10 OF 14 VANILLAJS CHALLENGE.js
+++ Assignment/DAY 10 OF 14 VANILLAJS CHALLENGE.js
@@ -44,3 +44,75 @@ if (range) {
 function init() {}
 
 init();
+
+
+// 정답
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Parcel Sandbox</title>
+    <meta charset="UTF-8" />
+  </head>
+
+  <body>
+    <h1>Random Number Game</h1>
+    <div>
+      <h3 class="js-title">Generate a number between 0 and <span>10</span></h3>
+      <datalist id="number">
+        <option value="50"></option>
+        <option value="100"></option>
+        <option value="150"></option>
+        <option value="200"></option>
+      </datalist>
+      <input
+        list="number"
+        id="js-range"
+        type="range"
+        min="5"
+        max="200"
+        value="5"
+        step="5"
+      />
+    </div>
+    <form id="js-guess">
+      <label>Guess the number:</label>
+      <input type="number" max="200" min="5" />
+      <button>Play!</button>
+    </form>
+    <div id="js-result">
+      <span></span>
+    </div>
+    <script src="src/index.js"></script>
+  </body>
+</html>
+
+
+const range = document.getElementById("js-range");
+const title = document.querySelector(".js-title");
+const guessForm = document.getElementById("js-guess");
+const result = document.getElementById("js-result");
+
+function handleRangeChange(e) {
+  const selectedRange = title.querySelector("span");
+  selectedRange.innerHTML = range.value;
+}
+
+function handleGuessSubmit(e) {
+  e.preventDefault();
+  const guessInput = guessForm.querySelector("input");
+  if (guessInput.value === "") {
+    return;
+  }
+  const max = range.value;
+  const random = Math.ceil(Math.random() * max);
+  const userGuess = parseInt(guessInput.value, 10);
+  const resultSpan = result.querySelector("span");
+  resultSpan.innerHTML = `
+  You chose: ${userGuess},
+  the machine chose: ${random}.<br />
+  <strong>${userGuess === random ? "You won!" : "You lost!"}</strong>
+  `;
+}
+
+guessForm.addEventListener("submit", handleGuessSubmit);
+range.addEventListener("input", handleRangeChange);
