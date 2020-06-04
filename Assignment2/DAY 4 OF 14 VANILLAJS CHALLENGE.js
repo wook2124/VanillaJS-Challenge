3fa6da3438d6959caf2034cbed04f604b9457615
Young <wook2124@naver.com>
2020-06-04 15:33:19 +0000

    DAY 4 OF 14 VANILLAJS CHALLENGE

---
+++ Assignment2/DAY 4 OF 14 VANILLAJS CHALLENGE.js
@@ -0,0 +1,25 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+const body = document.body;
+
+const BIG_SCREEN = "bigScreen";
+const MEDIUM_SCREEN = "mediumScreen";
+const SMALL_SCREEN = "smallScreen";
+
+function handleResize() {
+  const width = window.innerWidth;
+  if (width > 1000) {
+    body.classList.add(BIG_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN);
+  } else if (width <= 1000 && width >= 500) {
+    body.classList.add(MEDIUM_SCREEN);
+    body.classList.remove(BIG_SCREEN, SMALL_SCREEN);
+  } else {
+    body.classList.add(SMALL_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN);
+  }
+}
+
+window.addEventListener("resize", handleResize);
